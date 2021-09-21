e5383af8ba0068e2d4fa1bbac196ff039c4c4592
profemesti <90918399+profemesti@users.noreply.github.com>
2021-09-21 06:30:38 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int a,b,s;
+    std::cout << "Inserisci il primo numero " << std::endl;
+    std::cin>>a;
+    std::cout << "Inserisci il secondo numero " << std::endl;
+    std::cin>>b;
+    s=a+b;
+    std::cout << "La somma e'  " <<s << std::endl;
+    return 0;
+}
